58a6769783e01b683d5485ce61016fa7f313c925
Hoa Truong <anhoapitago@gmail.com>
2024-10-17 07:50:13 +0700

    Topic 05 XPath Tech

--- selenium/webdriver/Topic_02_Locator.java
+++ selenium/webdriver/Topic_02_Locator.java
@@ -8,6 +8,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+
 public class Topic_02_Locator {
     WebDriver driver;
 
@@ -19,9 +20,26 @@ public class Topic_02_Locator {
 
 
     @Test
-    public void TC_01_() {
+    public void TC_01_Find_Locator() {
         driver.get("https://alada.vn/tai-khoan/dang-ky.html");
         //Tìm khóa học
+
+        //non-XPath
+        driver.findElement(By.id("txtFirstname"));
+
+        //driver.findElement(By.className("btn_pink_sm fs16")); //not work because Selenium not support class name with space
+        //driver.findElement(By.className("text form-control")); //not work because Selenium not support class name with space
+        driver.findElement(By.className("btn_face")); //work
+
+        driver.findElement(By.name("txtFirstname"));
+
+//        driver.findElement(By.linkText("https://alada.vn/chinh-sach-bao-mat.html")); //not work because it get the visual text, not URL. link text as you see it with your eyes on the website (after all CSS is applied).
+        driver.findElement(By.linkText("chính sách")); //work
+        driver.findElement(By.partialLinkText("chính"));
+        driver.findElement(By.tagName("input"));
+        driver.findElement(By.cssSelector("input[id='txtFirstname']"));
+
+        //XPath
         driver.findElement(By.xpath("//input[@id='txtSearch']"));
 
         //Đăng ký
