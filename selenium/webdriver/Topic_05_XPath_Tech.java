8939c17fc999bf373c0bddf03d12226428921948
Hoa Truong <anhoapitago@gmail.com>
2024-10-18 16:41:17 +0700

    Topic 05

--- selenium/webdriver/Topic_05_XPath_Tech.java
+++ selenium/webdriver/Topic_05_XPath_Tech.java
@@ -36,6 +36,7 @@ public class Topic_05_XPath_Tech {
         driver.findElement(By.xpath("//h5[contains(.,'@04:45 PM')]"));
 
         //Michael Jackson
+        driver.findElement(By.xpath("//h5[contains(string(),'@04:45 PM')]"));
         driver.findElement(By.xpath("//h5[contains(.,'@04:45 PM')]"));
         driver.findElement(By.xpath("//h5[contains(string(),'Michael Jackson')]"));
         driver.findElement(By.xpath("//h5[contains(.,'Michael Jackson')]"));
@@ -49,11 +50,91 @@ public class Topic_05_XPath_Tech {
         driver.findElement(By.xpath(xPath1));
 
         driver.findElement(By.xpath("//p[contains(text(),'s Check or money order to:')]"));
-//        driver.findElement(By.xpath("p[text()=concat(\"Mail Personal or Business Check, Cashier'\",\"s Check or money order to:\")]"));
 
 
+        //Hello "John", What's happened?
+        //1
+        driver.findElement(By.xpath("//span[text()=concat('Hello \"John\", What',\"'s happened?\")]"));
+
+        //2
+        String xPath2 = """         
+                 //span[text()=concat('Hello "John", What',"'s happened?")]""";
+        driver.findElement(By.xpath(xPath2));
+
+        //3
+        driver.findElement(By.xpath("//span[@class = 'concat' and contains(text(),'Hello')]"));
+
+        //4
+        driver.findElement(By.xpath("//span[@class = 'concat' or contains(text(),'Hello')]"));
+
+        //5
+        driver.findElement(By.xpath("//span[@class = 'concat' and not(contains(text(),'teng teng'))]"));
     }
 
+    @Test
+    public void TC_02_XPath_With_Position() {
+        driver.get("https://automationfc.github.io/jquery-selectable/");
+
+        //get a number box
+        //1
+        driver.findElement(By.xpath("//li[1]"));
+        driver.findElement(By.xpath("//li[5]"));
+
+        //2
+        driver.findElement(By.xpath("//li[position()=10]"));
+
+        //3
+        driver.findElement(By.xpath("//li[last()]"));
+        driver.findElement(By.xpath("//li[last()-1]"));
+
+        //4
+        driver.findElement(By.xpath("//li[count(//li)]"));
+        driver.findElement(By.xpath("//li[count(//li)-1]"));
+        driver.findElement(By.xpath("//li[count(//li[@class='ui-state-default ui-selectee'])]"));
+        driver.findElement(By.xpath("//li[count(//li[@class='ui-state-default ui-selectee'])-1]"));
+    }
+
+    @Test
+    public void TC_02_XPath_With_Position2() {
+        driver.get("https://automationfc.github.io/basic-form/");
+
+        //<span class="singer"></span>
+        //1
+        driver.findElement(By.xpath("//span[@class='singer']")); // 6 elements
+        driver.findElement(By.xpath("(//span[@class='singer'])")); // 6 elements
+
+        //2
+        driver.findElement(By.xpath("//span[@class='singer'][1]")); // 1 element
+        driver.findElement(By.xpath("//span[@class='singer'][2]")); // no result because each span belong to a different parent node
+        driver.findElement(By.xpath("(//span[@class='singer'])[1]")); // 1 element
+        driver.findElement(By.xpath("(//span[@class='singer'])[2]")); // 1 element because those elements warped in () and count as child of the root, and there are 6 of them
+    }
+
+    @Test
+    public void TC_03_XPath_With_AXES_01() {
+        driver.get("https://www.packtpub.com/en-us/search?query=selenium");
+
+        //Book "Selenium Testing Tools Cookbook" --> Add to cart
+        //1
+        driver.findElement(By.xpath("//div[normalize-space(text()) ='Selenium Testing Tools Cookbook']//ancestor::div[@class='product-card-v2-content']//following-sibling::div//button[contains(@class,'cart-button')]"));
+
+        //Book "Learn Automation Testing with Java and Selenium Webdriver" --> Add to cart
+        //1
+        driver.findElement(By.xpath("//div[normalize-space(text()) ='Learn Automation Testing with Java and Selenium Webdriver']//ancestor::div[@class='product-card-v2-content']//following-sibling::div//button[contains(@class,'cart-button')]"));
+    }
+
+    @Test
+    public void TC_03_XPath_With_AXES_02() {
+        driver.get("https://www.packtpub.com/en-us/search?query=selenium");
+
+        //Book "Selenium Testing Tools Cookbook" --> Add to cart
+        //1
+        driver.findElement(By.xpath("//div[normalize-space(text()) ='Selenium Testing Tools Cookbook']//ancestor::div[@class='product-card-v2-content']//following-sibling::div//button[contains(@class,'cart-button')]"));
+
+        //Book "Learn Automation Testing with Java and Selenium Webdriver" --> Add to cart
+        //1
+        driver.findElement(By.xpath("//div[normalize-space(text()) ='Learn Automation Testing with Java and Selenium Webdriver']//ancestor::div[@class='product-card-v2-content']//following-sibling::div//button[contains(@class,'cart-button')]"));
+    }
 
     @AfterClass
     public void afterClass() {
