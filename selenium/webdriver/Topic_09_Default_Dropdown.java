dd333bb89caad02bf6e7bddd1b85f3e1df18a71c
Hoa Truong <anhoapitago@gmail.com>
2024-10-25 05:51:32 +0700

    Topic_09_Default_Dropdown

--- selenium/webdriver/Topic_09_Default_Dropdown.java
+++ selenium/webdriver/Topic_09_Default_Dropdown.java
@@ -3,8 +3,8 @@ package webdriver;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
@@ -20,21 +20,28 @@ public class Topic_09_Default_Dropdown {
     WebDriver driver;
     Random rand;
     String firstName, lastName, email, password;
+    String day = "31", month = "December", year = "2000";
 
     @BeforeClass
     public void beforeClass() {
+        Cookie cloudflareCookie = new Cookie("cf_clearance", "HMxyz9ukaddnBgmVs5eDg3RNFdCMCw6eSMu9D1HqzbM-1729760948-1.2.1.1-Bv4.Xsed3GjCsXffvMAdG6iIp3PcdtwXj5WgRAUTBQhXOsGNuXP.XoqA6Jz0w2TLcMCLvZtAIA.XFS_vNXtVJt8tYMga2KezbKxBpJQ8wTTL15uJtfOqhT4nK4FBnSy3vdT.cPlvw3sjKYAswV9f_Z1E0Mn6WEqlImUTrpj3xWFOcZFdhjsHXmBtiib43EAsQP4pFbLJ0hcJ5cWhl7EjIG62VQB6ec9_dbsCp4.oNbfQ155X0U3Yp8Gjdx6EijyrZiJOHTY0qmoTdlxF.Uu2_tvP08NzfQnpKSgk4rdI0Z5XpKAAJpoxEmXTjMk5vjzuP32zC0sL6z8e9HDHEAGoycFc5gxeazH.mN.JeTAcKBbHIHUnjfVtLZqOqzzFAmUHhQJYOfEQhiWMGGCZXZ6XkQ");
+
         driver = new FirefoxDriver();
-//        driver = new ChromeDriver();
+        driver.get("https://demo.nopcommerce.com/");
+        driver.manage().addCookie(cloudflareCookie);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         driver.manage().window().maximize();
+        driver.navigate().refresh();
     }
 
     @Test
     public void TC_01_Register() {
         //Exercise link: https://docs.google.com/document/d/1QRI6jdKoCiMB3K7s16f3jEtAVHICdROpw_t30RD8gac/edit?tab=t.0#heading=h.48we03cvzjbk
 
+
+
         //Step 01 - Truy cập vào trang: https://demo.nopcommerce.com
-        driver.get("https://demo.nopcommerce.com/register?returnUrl=%2F");
+        driver.get("https://demo.nopcommerce.com/");
 
         //Step 02 - Click Register link trên Header
         driver.findElement(By.cssSelector("a[class='ico-register']")).click();
@@ -70,11 +77,9 @@ public class Topic_09_Default_Dropdown {
         Assert.assertEquals(monthDropList.getOptions().size(),13);
         Assert.assertEquals(yearDropList.getOptions().size(),112);
 
-        dayDropList.selectByVisibleText("31");
-        monthDropList.selectByVisibleText("December");
-        yearDropList.selectByVisibleText("2000");
-
-
+        dayDropList.selectByVisibleText(day);
+        monthDropList.selectByVisibleText(month);
+        yearDropList.selectByVisibleText(year);
 
         //Email
         driver.findElement(By.cssSelector("input[id='Email']")).sendKeys(email);
@@ -87,8 +92,19 @@ public class Topic_09_Default_Dropdown {
         driver.findElement(By.cssSelector("button[id='register-button']")).click();
 
         //Step 05 - Verify đã vào trang Home Page sau khi đăng kí thành công
+        Assert.assertEquals(driver.getCurrentUrl(),"https://demo.nopcommerce.com/registerresult/1?returnUrl=/");
+        Assert.assertTrue(driver.findElement(By.cssSelector("div[class='result']")).isDisplayed());
 
         //Step 06 - Click vào trang My Account và kiểm tra ngày/ tháng/ năm nhập vào là đúng
+        driver.findElement(By.cssSelector("a[class='ico-account']")).click();
+
+        Assert.assertTrue(driver.findElement(By.cssSelector("input[id='gender-male']")).isSelected());
+        Assert.assertEquals(driver.findElement(By.cssSelector("input[id='FirstName']")).getAttribute("value"),firstName);
+        Assert.assertEquals(driver.findElement(By.cssSelector("input[id='LastName']")).getAttribute("value"),lastName);
+        Assert.assertEquals(new Select(driver.findElement(By.cssSelector("select[name='DateOfBirthDay']"))).getFirstSelectedOption().getText(),day);
+        Assert.assertEquals(new Select(driver.findElement(By.cssSelector("select[name='DateOfBirthMonth']"))).getFirstSelectedOption().getText(),month);
+        Assert.assertEquals(new Select(driver.findElement(By.cssSelector("select[name='DateOfBirthYear']"))).getFirstSelectedOption().getText(),year);
+        Assert.assertEquals(driver.findElement(By.cssSelector("input[id='Email']")).getAttribute("value"),email);
 
     }
 
